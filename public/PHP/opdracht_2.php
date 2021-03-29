a5f883743aae4b5ab6d2122be5325a64ac434d6b
Timvankooten <151274@csgaugustinus.nl>
2021-03-29 11:00:06 +0000

    29-3

--- public/PHP/opdracht_2.php
+++ public/PHP/opdracht_2.php
@@ -9,6 +9,11 @@ $tekst="Ik heb een bijbaantje bij de Aldi. De Aldi betaalt goed.";
 echo $tekst;
 $tekst=str_replace('Aldi','Albert Heijn',$tekst);
 echo "<h1> $tekst </h1>";
+$tekst=str_replace('Albert Heijn','Aldi',$tekst);
+echo "<h4>".$tekst."</h4>";
+
+$lengte = strlen($tekst); 
+echo "<I> De string $tekst bestaat uit </I>".$lengte." karakters.";
 
 /****************************
 EINDE VAN JOUW PHPCODE
